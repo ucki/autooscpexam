8a2c6e1c0a55a8ea08f2864eb02e4929b449343a
ucki <ucki@users.noreply.github.com>
2017-04-01 10:17:56 +0200

    Add files via upload

---
+++ oscpautopwn.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+echo " Welcome to: 
+          _______  _       _________   _______          _________ _______ 
+|\     /|(  ____ \| \    /\\__   __/  (  ___  )|\     /|\__   __/(  ___  )
+| )   ( || (    \/|  \  / /   ) (     | (   ) || )   ( |   ) (   | (   ) |
+| |   | || |      |  (_/ /    | |     | (___) || |   | |   | |   | |   | |
+| |   | || |      |   _ (     | |     |  ___  || |   | |   | |   | |   | |
+| |   | || |      |  ( \ \    | |     | (   ) || |   | |   | |   | |   | |
+| (___) || (____/\|  /  \ \___) (___  | )   ( || (___) |   | |   | (___) |
+(_______)(_______/|_/    \/\_______/  |/     \|(_______)   )_(   (_______)
+                                                                          
+ _______  _______  _______  _______ 
+(  ___  )(  ____ \(  ____ \(  ____ )
+| (   ) || (    \/| (    \/| (    )|
+| |   | || (_____ | |      | (____)|
+| |   | |(_____  )| |      |  _____)
+| |   | |      ) || |      | (      
+| (___) |/\____) || (____/\| )      
+(_______)\_______)(_______/|/ 
+
+Thanks to all of you boys & girls for the input ...
+I hope you enjoy this auto exam script .. unfortunately the runtime of this script is at the moment 25h .. I hope I can trim it down to run a bit faster .... Right now it doesn't violate any off sec rules .. hope it stays so ... so enjoy ...."
+sleep 30
+
+ for i in `seq 1 1800`;
+        do
+                b=$(awk -v min=1 -v max=48 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+	
+               
+		
+		if [ $b \< 10 ]; then
+               		echo "Try Harder"
+           	 fi
+if [ $b = 42 ]; then
+		echo "88     eeee "
+		echo "88        8" 
+		echo "88  88    8" 
+		echo "88ee88 eee8" 
+		echo "   88 8 "   
+		echo "   88 8eee "
+           	 fi
+if [ $b = 23 ]; then
+               		echo "23 – Nichts ist so wie es scheint"
+           	 fi
+if [ $b \< 20  ]; then
+               		echo "Enumerate"
+           	 fi
+if [ $b = 21  ]; then
+               		echo "Does not compute"
+           	 fi
+if [ $b = 22  ]; then
+               		echo "use <spoiler>"
+           	 fi
+if [ $b = 24  ]; then
+               		echo "Just running metasploit against all targets"
+           	 fi
+if [ $b = 25  ]; then
+               		echo "Solving google captchas because to much exploit googling"
+           	 fi
+if [ $b = 26  ]; then
+               		echo "Shall we Play a game?"
+           	 fi
+if [ $b = 27  ]; then
+               		echo "I know I've made some very poor decisions recently, but I can give you my complete assurance that my work will be back to normal. I've still got the greatest enthusiasm and confidence in the mission. And I want to help you."
+           	 fi
+if [ $b = 28  ]; then
+               		echo "I'll be back."
+           	 fi
+if [ $b = 29  ]; then
+               		echo "I can't do that Dave"
+           	 fi
+if [ $b = 30  ]; then
+               		echo "Hello, Mr. Anderson"
+           	 fi
+
+if [ $b = 31  ]; then
+               		echo "Hagel und Granaten!"
+           	 fi
+if [ $b = 32  ]; then
+               		echo "Alle Höllenhunde!"
+           	 fi
+if [ $b = 33  ]; then
+               		echo "Pirat! … Verbrecher! … Halsabschneider!"
+           	 fi
+if [ $b = 34  ]; then
+               		echo "Süsswassermatrosen! …"
+           	 fi
+if [ $b = 35  ]; then
+               		echo "Gummibeiniger Satansbraten!"
+           	 fi
+if [ $b = 36  ]; then
+               		echo "Sie zoologisches Abfallprodukt!"
+           	 fi
+if [ $b = 37  ]; then
+               		echo "Politiker!"
+           	 fi
+if [ $b = 38  ]; then
+               		echo "Sie Mückenhirn in Aspik!"
+           	 fi
+if [ $b = 39  ]; then
+               		
+echo "___________                 ___ ___                  .___             ._.
+\__    ___/______ ___.__.  /   |   \_____ _______  __| _/___________  | |
+  |    |  \_  __ <   |  | /    ~    \__  \\_  __ \/ __ |/ __ \_  __ \ | |
+  |    |   |  | \/\___  | \    Y    // __ \|  | \/ /_/ \  ___/|  | \/  \|
+  |____|   |__|   / ____|  \___|_  /(____  /__|  \____ |\___  >__|     __
+                  \/             \/      \/           \/    \/         \/"
+
+           	 fi
+if [ $b = 40  ]; then
+               		echo "_________ _______          
+\__   __/(  ____ )|\     /|
+   ) (   | (    )|( \   / )
+   | |   | (____)| \ (_) / 
+   | |   |     __)  \   /  
+   | |   | (\ (      ) (   
+   | |   | ) \ \__   | |   
+   )_(   |/   \__/   \_/   
+                           
+          _______  _______  ______   _______  _______    _ 
+|\     /|(  ___  )(  ____ )(  __  \ (  ____ \(  ____ )  ( )
+| )   ( || (   ) || (    )|| (  \  )| (    \/| (    )|  | |
+| (___) || (___) || (____)|| |   ) || (__    | (____)|  | |
+|  ___  ||  ___  ||     __)| |   | ||  __)   |     __)  | |
+| (   ) || (   ) || (\ (   | |   ) || (      | (\ (     (_)
+| )   ( || )   ( || ) \ \__| (__/  )| (____/\| ) \ \__   _ 
+|/     \||/     \||/   \__/(______/ (_______/|/   \__/  (_)
+                                                           "
+           	 fi
+if [ $b = 41  ]; then
+               		echo "_/// _//////                
+     _//                    
+     _//    _/ _///_//   _//
+     _//     _//    _// _// 
+     _//     _//      _///  
+     _//     _//       _//  
+     _//    _///      _//   
+                    _//     
+_//     _//                      _//                  _//
+_//     _//                      _//                  _//
+_//     _//   _//    _/ _///     _//   _//    _/ _/// _//
+_////// _// _//  _//  _//    _// _// _/   _//  _//    _/ 
+_//     _//_//   _//  _//   _/   _//_///// _// _//    _/ 
+_//     _//_//   _//  _//   _/   _//_/         _//       
+_//     _//  _// _///_///    _// _//  _////   _///    _//
+                                                      "
+           	 fi
+if [ $b = 43  ]; then
+               		echo "proof cat.txt"
+           	 fi
+if [ $b = 44  ]; then
+               		echo "locate cat.txt"
+           	 fi
+if [ $b = 45  ]; then
+               		echo "using loophohles"
+           	 fi
+if [ $b = 46  ]; then
+               		echo "look a local shell .... "
+           	 fi
+if [ $b = 47  ]; then
+               		echo "someone got a root shell ...."
+           	 fi
+if [ $b = 48  ]; then
+               		echo "Hätte hätte Fahrradkette ...."
+           	 fi
+
+	sleep 5
+        done    
